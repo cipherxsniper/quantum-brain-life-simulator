57a5361e970e437e4451393f84d1dcf4d8cbd864
cipherxsniper <simpl3hoods@gmail.com>
2026-01-06 05:20:07 -0800

    Replace Visualize with ArtSimulator in BrainLoop

--- BrainLoop.js
+++ BrainLoop.js
@@ -1,12 +1,12 @@
 import { SensoryInput } from '../sensory/SensoryInput.js';
 import { Attention } from '../attention/Attention.js';
-import { Visualize } from '../visualization/Visualize.js';
+import { ArtSimulator } from '../visualization/ArtSimulator.js';
 
 export class BrainLoop {
     constructor() {
         this.sensory = new SensoryInput();
         this.attention = new Attention();
-        this.visual = new Visualize();
+        this.visual = new ArtSimulator();
         this.state = {
             memoryStore: { longTerm: [] }
         };
